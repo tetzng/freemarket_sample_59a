96320ba3b8b558e16b50631fbbe4fdeb0329ae99
tetzng <ffffffffffffunk@gmail.com>
2019-09-21 13:39:00 +0900

    Basic auth only production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
+  before_action :basic_auth, if: :production?
 
   private
+
+  def production?
+    Rails.env.production?
+  end
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
