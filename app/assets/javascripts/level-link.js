d5c16d3db7031d9acd83d7e64a02a2dfa6855e5a
seicode <nv7423@gmail.com>
2019-09-23 13:02:19 +0900

    creating header

---
+++ app/assets/javascripts/level-link.js
@@ -0,0 +1,36 @@
+// $(function(){
+//   $("ul.first-category__wrapper").hide();
+//   $("ul.toppage-header-top__footer-search-categories li").hover(function(){
+//     $(">ul:not(:animated)",this).slideDown("fast");
+//   },
+//   function(){
+//     $(">ul",this).slideUp("fast");
+//   });
+// });
+
+// $(function () {
+//   $(".first-category__wrapper").hide();
+//   $(".second-category__wrapper").hide();
+//   $(".third-category__wrapper").hide();
+
+//   $(".toppage-header-top__footer-search").hover(function () {
+//     console.log('1段目');
+//     $(".first-category__wrapper").show()
+//   }, function () {
+//     $(".first-category__wrapper").hide()
+//   });
+
+//   $(".first-category").hover(function () {
+//     console.log('2段目');
+//     $(this).children(".second-category__wrapper").show()
+//   }, function () {
+//     $(this).children(".second-category__wrapper").hide()
+//   });
+
+//   $(".second-category").hover(function () {
+//     console.log('3段目');
+//     $(this).children(".third-category__wrapper").show()
+//   }, function () {
+//     $(this).children(".third-category__wrapper").hide()
+//   });
+// });
