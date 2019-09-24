14dcdff551b30b456515f692972614319c8bba01
enahamada <chamaburiburi11.10@gmail.com>
2019-09-24 13:18:29 +0900

    controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'mypage_profile#index'
+  root 'toppage#index'
+  get '/mypage_profile', to: 'mypage_profile#index'
 end
