a1f44636382dfefa89b515055d32cc5d8185ac8a
seicode <nv7423@gmail.com>
2019-09-24 14:00:54 +0900

    [slider]installed slick

---
+++ app/assets/javascripts/slider.js
@@ -0,0 +1,27 @@
+// Slick(トップページスライダー)の設定
+// [Slick](https://github.com/kenwheeler/slick/)
+
+$(function() {
+  $('.toppage-slider__wrapper').slick({
+      // ドットにクラスを追加
+      dotsClass: 'slide-dots',
+      // ドットを追加する要素を指定
+      appendDots: $('.toppage-slider'),
+      // スライダー矢印の指定
+      prevArrow:'<i class="fas fa-chvron-left", id="prev-arrow"></i>',
+      nextArrow:'<i class="fas fa-chevron-right", id="next-arrow"></i>',
+      // ドットの有無
+      dots: true,
+      // 自動再生
+      autoplay: true,
+      autoplaySpeed: 4000,
+      speed: 800,
+      // スライダー内の画像の位置指定
+      centerMode: true,
+  });
+
+  // ドットの操作
+  $('.slide-dots li').on('mouseover', function() {
+    $('.slider').slick('goTo', $(this).index());
+  });
+});
