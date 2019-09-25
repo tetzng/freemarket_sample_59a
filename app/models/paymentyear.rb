c0e2b619e7a34b49cbcc75b92b164e5d0a6f83d9
tetzng <ffffffffffffunk@gmail.com>
2019-09-25 18:07:53 +0900

    Fix " to '

--- app/models/paymentyear.rb
+++ app/models/paymentyear.rb
@@ -1,16 +1,16 @@
 # クレカ有効期限(年)
 class Paymentyear < ActiveHash::Base
   self.data = [
-      { id: 1, num: "19" },
-      { id: 2, num: "20" },
-      { id: 3, num: "21" },
-      { id: 4, num: "22" },
-      { id: 5, num: "23" },
-      { id: 6, num: "24" },
-      { id: 7, num: "25" },
-      { id: 8, num: "26" },
-      { id: 9, num: "27" },
-      { id: 10, num: "28" },
-      { id: 11, num: "29" },
+      { id: 1, num: '19' },
+      { id: 2, num: '20' },
+      { id: 3, num: '21' },
+      { id: 4, num: '22' },
+      { id: 5, num: '23' },
+      { id: 6, num: '24' },
+      { id: 7, num: '25' },
+      { id: 8, num: '26' },
+      { id: 9, num: '27' },
+      { id: 10, num: '28' },
+      { id: 11, num: '29' },
     ]
 end
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -13,15 +13,15 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    'uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}'
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+  #   # ActionController::Base.helpers.asset_path('fallback/' + [version_name, 'default.png'].compact.join('_'))
   #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+  #   '/images/fallback/' + [version_name, 'default.png'].compact.join('_')
   # end
 
   # Process files as they are uploaded:
@@ -45,6 +45,6 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   # def filename
-  #   "something.jpg" if original_filename
+  #   'something.jpg' if original_filename
   # end
 end
