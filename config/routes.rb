f387fba18a8bcd6593b28058d183a3b06a12f286
tetzng <ffffffffffffunk@gmail.com>
2019-09-25 16:42:54 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  get '/signup', to: 'signup#index'
+  get '/signup/registration', to: 'signup#registration'
+  get '/signup/sms_confirmation', to: 'signup#sms_confirmation'
+  get '/signup/sms_confirmation/sms', to: 'signup#sms_confirmation_sms'
+  get '/signup/address', to: 'signup#address'
+  get '/signup/credit_card', to: 'signup#credit_card'
+  get '/signup/done', to: 'signup#done'
+  get '/login', to: 'signup#login'
   root 'toppage#index'
   get 'mypage/identification'
   get '/mypage', to: 'mypage#index'
   get '/mypage/profile', to: 'mypage#profile'
+  get '/logout', to: 'mypage#logout'
 end
