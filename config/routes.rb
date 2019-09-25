533e41c0a8ce56fb2464fe48ea1f551623b8f3ab
tetzng <ffffffffffffunk@gmail.com>
2019-09-25 21:51:39 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   get 'toppage/buydetails'
   get 'mypage/identification'
   get '/mypage', to: 'mypage#index'
+  get 'mypage/card'
   get '/mypage/profile', to: 'mypage#profile'
   get '/logout', to: 'mypage#logout'
 end
