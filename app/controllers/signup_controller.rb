7a242624628ab77a88b177fe6ed57170891b01b1
tetzng <ffffffffffffunk@gmail.com>
2019-09-25 16:41:53 +0900

    Create signup_controller.rb

---
+++ app/controllers/signup_controller.rb
@@ -0,0 +1,18 @@
+class SignupController < ApplicationController
+  def index
+  end
+  def registration
+  end
+  def sms_confirmation
+  end
+  def sms_confirmation_sms
+  end
+  def address
+  end
+  def credit_card
+  end
+  def done
+  end
+  def login
+  end
+end
