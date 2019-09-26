3e06ba38eb226ebc674d3911944d36fe4fca093a
yuuspe <pzugn@yahoo.co.jp>
2019-09-26 14:21:45 +0900

    Create users.rb

---
+++ spec/factories/users.rb
@@ -0,0 +1,17 @@
+FactoryBot.define do
+
+  factory :user do
+    nickname {"abe"}
+    first_name {"abe"}
+    last_name {"abe"}
+    first_name_kana {"ア"}
+    last_name_kana {"ア"}
+    birthday_year {"1990""}
+    birthday_month {""}
+    birthday_day {""}
+    phone_num {"080-8654-7678"}
+    authentication_num {"0000"}
+    content {"abe"}
+  end
+
+end
