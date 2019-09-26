b66040044f5e6ae609836061ed45995c301fe629
kurachitoshiaki <aru0220kuria0214@gmail.com>
2019-09-26 19:06:34 +0900

    Added gem owlcarousel

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-
+//= require owl.carousel
 //= require jquery3
 //= require rails-ujs
 //= require activestorage
