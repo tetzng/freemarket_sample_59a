25e64407f32b1a997d74824467dc37842d78febe
yuuspe <pzugn@yahoo.co.jp>
2019-09-26 14:21:47 +0900

    Create user_spec.rb

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe User do
+  describe '#create' do
+
+    it "is invalid without a nickname" do
+      user = build(:user, nickname: "")
+      user.valid?
+      expect(user.errors[:nickname]).to include("can't be blank")
+    end
+
+    it "is invalid without a email" do
+      user = build(:user, email: "")
+      user.valid?
+      expect(user.errors[:email]).to include("can't be blank")
+    end
+
+  end
+end
