f5a70c3eb87c62ec4e20b5417195fd1dfaa73118
yuuspe <pzugn@yahoo.co.jp>
2019-09-27 16:24:52 +0900

    Update 20190924081726_add_nickname_to_users.rb

--- db/migrate/20190924081726_add_nickname_to_users.rb
+++ db/migrate/20190924081726_add_nickname_to_users.rb
@@ -1,6 +1,5 @@
 class AddNicknameToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :nickname, :string, null: false
     add_column :users, :first_name, :string, null: false
     add_column :users, :last_name, :string, null:false
     add_column :users, :first_name_kana, :string, null:false
