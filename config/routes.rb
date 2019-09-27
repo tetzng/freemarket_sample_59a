2ae48b3f490752b623b49480bd3c065f51cb79fb
enahamada <chamaburiburi11.10@gmail.com>
2019-09-27 12:23:00 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -19,4 +19,10 @@ Rails.application.routes.draw do
   get '/mypage/card/create', to: 'mypage#card_create'
   get '/mypage/profile', to: 'mypage#profile'
   get '/logout', to: 'mypage#logout'
+
+
+  # devise_for :users, controllers: {
+  #   omniauth_callbacks: "users/omniauth_callbacks"
+  # }
+
 end
