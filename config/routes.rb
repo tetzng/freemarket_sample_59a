6ddd1740d6df83db9a6a95032b187af29662ac6a
enahamada <chamaburiburi11.10@gmail.com>
2019-09-27 12:23:00 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    omniauth_callbacks: "users/omniauth_callbacks"
+  }
 
   resources :card, only: [:new, :show] do
     collection do
