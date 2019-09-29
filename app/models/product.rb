c96e20440693f2dc7b312e5b2b0aa3219fc3e2d5
enahamada <chamaburiburi11.10@gmail.com>
2019-09-29 12:26:50 +0900

    commit

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,2 @@
-# クレカ有効期限(年)とアソシエーション
 class Product < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :paymentyear
 end
--- db/migrate/20190929030615_create_products.rb
+++ db/migrate/20190929030615_create_products.rb
@@ -1,9 +1,7 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
-      t.integer :paymentyear_id
-      t.string :num
-
+    
       t.timestamps
     end
   end
