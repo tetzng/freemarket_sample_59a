e361d47a160d48ec5f52f872a60d4af10ab87c98
yuuspe <pzugn@yahoo.co.jp>
2019-09-29 11:57:22 +0900

    Update birth_day.rb

--- app/models/birth_day.rb
+++ app/models/birth_day.rb
@@ -1,5 +1,5 @@
 # 生年月日の(日)とアソシエーション
 class BirthDay < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :birth_dd
+  # extend ActiveHash::Associations::ActiveRecordExtensions
+  # belongs_to_active_hash :birth_dd
 end
