f9d40b882e99f99e9e6db1697a22525139e87308
enahamada <chamaburiburi11.10@gmail.com>
2019-09-30 13:55:50 +0900

    commit

--- app/controllers/sell_controller.rb
+++ app/controllers/sell_controller.rb
@@ -14,12 +14,11 @@ class SellController < ApplicationController
   private
 
   def product_params
-    params.permit(:image, :name, :)
-    
+    params.permit(:image, :name, :description, :category_id, :condition_id, :size_id, :brand, :delivery_charge, :delivery_way, :delivery_area, :delivery_days, :price, :status)
   end
 
-  def move_to_index
-    redirect_to :action => 'index' unless user_signed_in?
-  end
+  # def move_to_index
+  #   redirect_to :action => 'index' unless user_signed_in?
+  # end
 
 end 
