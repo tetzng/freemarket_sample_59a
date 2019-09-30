1b78fd8acbf326c0767fa62cdaef8d4965b7b151
yuuspe <pzugn@yahoo.co.jp>
2019-09-30 18:56:02 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,50 +18,48 @@ class User < ApplicationRecord
   # has_one_attached :avatar
   # has_many :likes, through: :like_users
 
-#   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-#   VALID_PHONE_REGEX = /\A\d{10}$|^\d{11}\z/
-#   VALID_KATAKANA_REGEX = /\A[\p{katakana}\p{blank}ー－]+\z/
-#   VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_PHONE_REGEX = /\A\d{10}$|^\d{11}\z/
+  VALID_KATAKANA_REGEX = /\A[\p{katakana}\p{blank}ー－]+\z/
+  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+  VALID_POSTAL_CODE = /\A\d{3}-\d{4}\z/i
 
-# registration
-#   validates :nickname, presence: true, length: { maximum: 20 }
-#   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX, message: 'のフォーマットが不適切です'}
-#   validates :password, presence: true, length: { in: 7..128 }, format: { with: VALID_PASSWORD_REGEX , message: 'は英字と数字両方を含むパスワードを設定してください'}
-#   validates :password_confirmation, presence: true, length: { in: 7..128 }, format: { with: VALID_PASSWORD_REGEX, message: 'は英字と数字両方を含むパスワードを設定してください'}
-#   validates :last_name, presence: true, length: { maximum: 35 }
-#   validates :first_name, presence: true, length: { maximum: 35 }
-#   validates :last_name_kana, presence: true, length: { maximum: 35 }, format: { with: VALID_KATAKANA_REGEX, message: 'はカタカナで入力して下さい'}
-#   validates :first_name_kana, presence: true, length: { maximum: 35 }, format: { with: VALID_KATAKANA_REGEX, message: 'はカタカナで入力して下さい'}
-#   validates :birth_yyyy_id, presence: true
-#   validates :birth_mm_id, presence: true
-#   validates :birth_dd_id, presence: true
+#registration
+  validates :nickname, presence: true, length: { maximum: 20 }
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX, message: 'のフォーマットが不適切です'}
+  validates :password, presence: true, length: { in: 7..128 }, format: { with: VALID_PASSWORD_REGEX , message: 'は英字と数字両方を含むパスワードを設定してください'}
+  validates :password_confirmation, presence: true, length: { in: 7..128 }, format: { with: VALID_PASSWORD_REGEX, message: 'は英字と数字両方を含むパスワードを設定してください'}
+  validates :last_name, presence: true, length: { maximum: 35 }
+  validates :first_name, presence: true, length: { maximum: 35 }
+  validates :last_name_kana, presence: true, length: { maximum: 35 }, format: { with: VALID_KATAKANA_REGEX, message: 'はカタカナで入力して下さい'}
+  validates :first_name_kana, presence: true, length: { maximum: 35 }, format: { with: VALID_KATAKANA_REGEX, message: 'はカタカナで入力して下さい'}
+  validates :birth_yyyy_id, presence: true
+  validates :birth_mm_id, presence: true
+  validates :birth_dd_id, presence: true
 
-# #sms_confirmation
-#   validates :phone_num, presence: true, format: { with: /\A\d{10,11}\z/, message: 'の入力が正しくありません'}
+#sms_confirmation
+  validates :phone_num, presence: true, format: { with: /\A\d{10,11}\z/, message: 'の入力が正しくありません'}
 
-# #sms_confirmation/sms
-#   validates :authentication_num, presence: true,numericality: { only_integer: true }
-# #signup/address
-#   validates :last_name, presence: true, length: { maximum: 35 }
-#   validates :first_name, presence: true, length: { maximum: 35 }
-#   validates :last_name_kana, presence: true, length: { maximum: 35 }, format: { with: VALID_KATAKANA_REGEX, message: 'はカタカナで入力して下さい'}
-#   validates :first_name_kana, presence: true, length: { maximum: 35 }, format: { with: VALID_KATAKANA_REGEX, message: 'はカタカナで入力して下さい'}
-#   validates :zip_code1, presence: true, length: { maximum: 8 }
-#   validates :prefecture_id, presence: true, length: { maximum: 8 }
-#   validates :city, presence: true, length: { maximum: 8 }
-#   validates :address1, presence: true, length: { maximum: 8 }
-#   validates :address2, presence: true, length: { maximum: 8 }
-#   validates :telephone, presence: true, length: { maximum: 8 }
+#sms_confirmation/sms
+  validates :authentication_num, presence: true, numericality: { only_integer: true }
 
+#signup/address
+  validates :last_name, presence: true, length: { maximum: 35 }
+  validates :first_name, presence: true, length: { maximum: 35 }
+  validates :last_name_kana, presence: true, length: { maximum: 35 }, format: { with: VALID_KATAKANA_REGEX, message: 'はカタカナで入力して下さい'}
+  validates :first_name_kana, presence: true, length: { maximum: 35 }, format: { with: VALID_KATAKANA_REGEX, message: 'はカタカナで入力して下さい'}
+  validates :zip_code1, presence: true, length: { maximum: 8 }, format: { with: VALID_POSTAL_CODE, message: 'のフォーマットが不適切です' }
+  validates :prefecture_id, presence: true
+  validates :city, presence: true, length: { maximum: 50 }
+  validates :address1, presence: true, length: { maximum: 100 }
+  validates :address2, length: { maximum: 100 }
+  validates :telephone, length: { maximum: 8 }
 
-
-
-#   session[:zip_code1] = user_params[:zip_code1]
-#   session[:prefecture_id] = user_params[:prefecture_id]
-#   session[:city] = user_params[:city]
-#   session[:address1] = user_params[:address1]
-#   session[:address2] = user_params[:address2]
-#   session[:telephone] = user_params[:telephone]
+#signup/credit_card
+  validates :payment_card_no, presence: true, length: { maximum: 16 }, numericality: { only_integer: true }
+  validates :paymentmonth_id, presence: true
+  validates :paymentyear_id, presence: true
+  validates :payment_card_security_code, presence: true, length: { maximum: 4 }
 
 
 end
