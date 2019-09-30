aa335bd623e95fae566d18c6e7fd341f78a5672f
yuuspe <pzugn@yahoo.co.jp>
2019-09-30 18:55:58 +0900

    Update sessions_controller.rb

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -8,15 +8,15 @@ class Users::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  POST /resource/sign_in
+  def create
+    super
+  end
 
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  DELETE /resource/sign_out
+  def destroy
+    super
+  end
 
   # protected
 
