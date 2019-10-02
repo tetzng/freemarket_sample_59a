3205ca5716cdbfd90601d74601fe4c8e12a89423
seicode <nv7423@gmail.com>
2019-10-02 11:52:42 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
 
-  # devise_for :users, controllers: {
-  #   omniauth_callbacks: "users/omniauth_callbacks",
-  #   sessions: 'users/sessions'
-  # }
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
   resources :sell
   resources :users
 
