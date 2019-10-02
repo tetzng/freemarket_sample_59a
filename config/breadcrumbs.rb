71bceebfd07edccee04dd5bd05e56c537e23bc56
kurachitoshiaki <aru0220kuria0214@gmail.com>
2019-10-02 11:23:45 +0900

    Update breadcrumbs.rb

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -18,7 +18,7 @@ crumb :mypage_identification do
 end
 
 crumb :itembuy do |item|
-  link item.name, toppage_itembuy_path(products.id)
+  link item.name, toppage_itembuy_path(item)
   parent :root
 end
 
@@ -41,19 +41,3 @@ crumb :sell_item do |q|
   link '出品した商品'
   parent :mypage
 end
-
-# crumb :project_issues do |project|
-#   link 'Issues', project_issues_path(project)
-#   parent :project, project
-# end
-
-# crumb :issue do |issue|
-#   link issue.title, issue_path(issue)
-#   parent :project_issues, issue.project
-# end
-
-# If you want to split your breadcrumbs configuration over multiple files, you
-# can create a folder named `config/breadcrumbs` and put your configuration
-# files there. All *.rb files (e.g. `frontend.rb` or `products.rb`) in that
-# folder are loaded and reloaded automatically when you change them, just like
-# this file (`config/breadcrumbs.rb`).
