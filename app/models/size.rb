5b07ea051a62a8c50d61581ea8d3d574477406e6
tetzng <ffffffffffffunk@gmail.com>
2019-10-03 17:25:04 +0900

    Update size.rb

--- app/models/size.rb
+++ app/models/size.rb
@@ -13,3 +13,20 @@ class Size < ActiveHash::Base
       { id: 10, value: 'FREE SIZE' }
   ]
 end
+
+# <option value="144">20cm以下</option>
+# <option value="145">20.5cm</option>
+# <option value="146">21cm</option>
+# <option value="147">21.5cm</option>
+# <option value="148">22cm</option>
+# <option value="16">22.5cm</option>
+# <option value="17">23cm</option>
+# <option value="18">23.5cm</option>
+# <option value="19">24cm</option>
+# <option value="20">24.5cm</option>
+# <option value="21">25cm</option>
+# <option value="22">25.5cm</option>
+# <option value="149">26cm</option>
+# <option value="150">26.5cm</option>
+# <option value="151">27cm</option>
+# <option value="152">27.5cm以上</option>
