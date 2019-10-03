8e5f24e6f366609a0f5345ec0fcd8db5429bef8f
tetzng <ffffffffffffunk@gmail.com>
2019-10-04 04:07:52 +0900

    Update size.rb

--- app/models/size.rb
+++ app/models/size.rb
@@ -1,32 +1,33 @@
 class Size < ActiveHash::Base
   include ActiveHash::Associations
   self.data = [
-      { id: 1, value: 'XXS以下' },
-      { id: 2, value: 'XS(SS)' },
-      { id: 3, value: 'S' },
-      { id: 4, value: 'M' },
-      { id: 5, value: 'L' },
-      { id: 6, value: 'XL(LL)' },
-      { id: 7, value: '2XL(3L)' },
-      { id: 8, value: '3XL(4L)' },
-      { id: 9, value: '4XL(5L)以上' },
-      { id: 10, value: 'FREE SIZE' }
+      { id: 1, value: 'XXS以下', group: '1' },
+      { id: 2, value: 'XS(SS)', group: '1' },
+      { id: 3, value: 'S', group: '1' },
+      { id: 4, value: 'M', group: '1' },
+      { id: 5, value: 'L', group: '1' },
+      { id: 6, value: 'XL(LL)', group: '1' },
+      { id: 7, value: '2XL(3L)', group: '1' },
+      { id: 8, value: '3XL(4L)', group: '1' },
+      { id: 9, value: '4XL(5L)以上', group: '1' },
+      { id: 10, value: 'FREE SIZE', group: '1' },
+      { id: 11, value: '20cm以下', group: '2' },
+      { id: 12, value: '20.5cm', group: '2' },
+      { id: 13, value: '21cm', group: '2' },
+      { id: 14, value: '21.5cm', group: '2' },
+      { id: 15, value: '22cm', group: '2' },
+      { id: 16, value: '22.5cm', group: '2' },
+      { id: 17, value: '23cm', group: '2' },
+      { id: 18, value: '23.5cm', group: '2' },
+      { id: 19, value: '24cm', group: '2' },
+      { id: 20, value: '24.5cm', group: '2' },
+      { id: 21, value: '25cm', group: '2' },
+      { id: 22, value: '25.5cm', group: '2' },
+      { id: 23, value: '26cm', group: '2' },
+      { id: 24, value: '26.5cm', group: '2' },
+      { id: 25, value: '27cm', group: '2' },
+      { id: 26, value: '27.5cm以上', group: '2' },
+      { id: 99, value: '', group: '' }
   ]
 end
 
-# <option value="144">20cm以下</option>
-# <option value="145">20.5cm</option>
-# <option value="146">21cm</option>
-# <option value="147">21.5cm</option>
-# <option value="148">22cm</option>
-# <option value="16">22.5cm</option>
-# <option value="17">23cm</option>
-# <option value="18">23.5cm</option>
-# <option value="19">24cm</option>
-# <option value="20">24.5cm</option>
-# <option value="21">25cm</option>
-# <option value="22">25.5cm</option>
-# <option value="149">26cm</option>
-# <option value="150">26.5cm</option>
-# <option value="151">27cm</option>
-# <option value="152">27.5cm以上</option>
