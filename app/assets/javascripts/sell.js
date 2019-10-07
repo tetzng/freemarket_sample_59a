e1d6bb9e50abcf8129f9828203b10b3f9db7074b
tetzng <ffffffffffffunk@gmail.com>
2019-10-07 14:25:41 +0900

    fix some bugs

--- app/assets/javascripts/sell.js
+++ app/assets/javascripts/sell.js
@@ -288,108 +288,107 @@ $(document).on('turbolinks:load', function(){
 
 
 // ドラッグアンドドロップの処理、書いてる途中
-// document.addEventListener('DOMContentLoaded', function () {
-//   var
-//     dropArea = document.getElementById('dropArea'),
-//     output = document.getElementById('output'),
+document.addEventListener('DOMContentLoaded', function () {
+  var
+    dropArea = document.getElementById('dropArea'),
+    output = document.getElementById('output'),
 
-//     // 画像の最大ファイルサイズ（20MB）
-//     maxSize = 20 * 1024 * 1024;
+    // 画像の最大ファイルサイズ（20MB）
+    maxSize = 20 * 1024 * 1024;
 
-//   // ドロップされたファイルの整理
-//   function organizeFiles(files) {
-//     var
-//       length = files.length,
-//       i = 0, file;
+  // ドロップされたファイルの整理
+  function organizeFiles(files) {
+    var
+      length = files.length,
+      i = 0, file;
 
-//     for (; i < length; i++) {
-//       // file には Fileオブジェクト というローカルのファイル情報を含むオブジェクトが入る
-//       file = files[i];
+    for (; i < length; i++) {
+      // file には Fileオブジェクト というローカルのファイル情報を含むオブジェクトが入る
+      file = files[i];
 
-//       // 画像以外は無視
-//       if (!file || file.type.indexOf('image/') < 0) {
-//         continue;
-//       }
+      // 画像以外は無視
+      if (!file || file.type.indexOf('image/') < 0) {
+        continue;
+      }
 
-//       // 指定したサイズを超える画像は無視
-//       if (file.size > maxSize) {
-//         continue;
-//       }
+      // 指定したサイズを超える画像は無視
+      if (file.size > maxSize) {
+        continue;
+      }
 
-//       // 画像出力処理へ進む
-//       outputImage(file);
-//     }
-//   }
+      // 画像出力処理へ進む
+      outputImage(file);
+    }
+  }
 
-//   // 画像の出力
-//   function outputImage(blob) {
-//     var
-//       // 画像要素の生成
-//       image = new Image(),
+  // 画像の出力
+  function outputImage(blob) {
+    var
+      // 画像要素の生成
+      image = new Image(),
 
-//       // File/BlobオブジェクトにアクセスできるURLを生成
-//       blobURL = URL.createObjectURL(blob);
+      // File/BlobオブジェクトにアクセスできるURLを生成
+      blobURL = URL.createObjectURL(blob);
 
-//     // src にURLを入れる
-//     image.src = blobURL;
+    // src にURLを入れる
+    image.src = blobURL;
 
-//     // 画像読み込み完了後
-//     image.addEventListener('load', function () {
-//       // File/BlobオブジェクトにアクセスできるURLを開放
-//       URL.revokeObjectURL(blobURL);
-//     var imageHTML = "<li class='sell-main__upload-item'><figure class='sell-main__upload-figure sell-main__upload-figure--square'>" + image + "</figure><div class='sell-main__upload-button'><a class='sell-main__upload-edit' href=''>編集</a><a href=''>削除</a></div></li>";
-//       // #output へ出力
-//     var itemList = document.createElement("li");
-//     itemList.setAttribute("class", "sell-main__upload-item");
-//     itemList.innerHTML     = "<figure class='sell-main__upload-figure sell-main__upload-figure--square'></figure><div class='sell-main__upload-button'><a class='sell-main__upload-edit' href=''>編集</a><a href=''>削除</a></div>";
-//     console.log(itemList);
-//       output.appendChild(itemList);
-//       var imageWrapper = output.lastChild.firstChild;
-//       console.log(output.lastChild.firstChild);
-//       console.log(image);
-//       imageWrapper.appendChild(image);
-//     });
-//   }
+    // 画像読み込み完了後
+    image.addEventListener('load', function () {
+      // File/BlobオブジェクトにアクセスできるURLを開放
+      URL.revokeObjectURL(blobURL);
+      var imageHTML = `<li class='sell-main__upload-item'><figure class='sell-main__upload-figure sell-main__upload-figure--square'></figure><div class='sell-main__upload-button'><a class='sell-main__upload-edit' href=''>編集</a><a class='sell-main__delete-image'>削除</a></div></li>`;
+      console.log(typeof imageHTML);
+      console.log(typeof image);
+      // #output へ出力
+      // var itemList = document.createElement("li");
+      // itemList.setAttribute("class", "sell-main__upload-item");
+      // itemList.innerHTML = "<figure class='sell-main__upload-figure sell-main__upload-figure--square'></figure><div class='sell-main__upload-button'><a class='sell-main__upload-edit' href=''>編集</a><a class='sell-main__delete-image'>削除</a></div>";
+      // itemList.innerHTML = imageHTML
+      output.appendChild(imageHTML);
+      var imageWrapper = output.lastChild.firstChild;
+      imageWrapper.appendChild(image);
+    });
+  }
 
-//   // ドラッグ中の要素がドロップ要素に重なった時
-//   dropArea.addEventListener('dragover', function (ev) {
-//     ev.preventDefault();
+  // ドラッグ中の要素がドロップ要素に重なった時
+  dropArea.addEventListener('dragover', function (ev) {
+    ev.preventDefault();
 
-//     // ファイルのコピーを渡すようにする
-//     ev.dataTransfer.dropEffect = 'copy';
+    // ファイルのコピーを渡すようにする
+    ev.dataTransfer.dropEffect = 'copy';
 
-//     dropArea.classList.add('dragover');
-//   });
+    dropArea.classList.add('dragover');
+  });
 
-//   // ドラッグ中の要素がドロップ要素から外れた時
-//   dropArea.addEventListener('dragleave', function () {
-//     dropArea.classList.remove('dragover');
-//   });
+  // ドラッグ中の要素がドロップ要素から外れた時
+  dropArea.addEventListener('dragleave', function () {
+    dropArea.classList.remove('dragover');
+  });
 
-//   // ドロップ要素にドロップされた時
-//   dropArea.addEventListener('drop', function (ev) {
-//     ev.preventDefault();
+  // ドロップ要素にドロップされた時
+  dropArea.addEventListener('drop', function (ev) {
+    ev.preventDefault();
 
-//     dropArea.classList.remove('dragover');
-//     output.textContent = '';
+    dropArea.classList.remove('dragover');
+    output.textContent = '';
+    // ev.dataTransfer.files に複数のファイルのリストが入っている
+    organizeFiles(ev.dataTransfer.files);
+  });
 
-//     // ev.dataTransfer.files に複数のファイルのリストが入っている
-//     organizeFiles(ev.dataTransfer.files);
-//   });
+  // // #dropArea がクリックされた時
+  // dropArea.addEventListener('click', function () {
+  //   fileInput.click();
+  // });
 
-//   // // #dropArea がクリックされた時
-//   // dropArea.addEventListener('click', function () {
-//   //   fileInput.click();
-//   // });
+  // ファイル参照で画像を追加した場合
+  // product_images.addEventListener('change', function (ev) {
+  //   output.textContent = '';
 
-//   // ファイル参照で画像を追加した場合
-//   product_images.addEventListener('change', function (ev) {
-//     output.textContent = '';
+  //   // ev.target.files に複数のファイルのリストが入っている
+  //   organizeFiles(ev.target.files);
 
-//     // ev.target.files に複数のファイルのリストが入っている
-//     organizeFiles(ev.target.files);
-
-//     // 値のリセット
-//     fileInput.value = '';
-//   });
-// });
+  //   // 値のリセット
+  //   fileInput.value = '';
+  // });
+});
