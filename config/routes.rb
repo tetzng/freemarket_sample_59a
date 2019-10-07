4c62a130daafb6ae7ee3b662bd2b88c40ef4c4d1
tetzng <ffffffffffffunk@gmail.com>
2019-10-08 03:05:37 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
+
   resources :card, only: [:new, :show] do
     collection do
-      post 'show', to: 'card#show'
-      post 'pay', to: 'card#pay'
-      post 'delete', to: 'card#delete'
+      post 'show'
+      post 'pay'
+      post 'delete'
     end
-  end
-  resources :signup, only: :create
-  get 'sell/buydetails', to: 'sell#buydetails'
-  resources :sell do
-    resources :purchase, only: [:show] do
-      collection do
-        get 'show', to: 'purchase#show'
-        post 'pay', to: 'purchase#pay'
-        get 'done', to: 'purchase#done'
-      end
-    end
-  end
-  get '/sell/new_size', to: 'sell#new_size'
-  resources :signup, only: :create
-  resources :sell
-  namespace :api do
-    resources :sell, only: :new, defaults: { format: 'json' }
-    get '/sell/new_delivery', to: 'sell#new_delivery', defaults: { format: 'json' }
   end
 
+  resources :signup, only: :create
   get '/signup', to: 'signup#index'
   get '/signup/registration', to: 'signup#registration'
   get '/signup/sms_confirmation', to: 'signup#sms_confirmation'
@@ -33,6 +17,22 @@ Rails.application.routes.draw do
   get '/signup/address', to: 'signup#address'
   get '/signup/credit_card', to: 'signup#credit_card'
   get '/signup/done', to: 'signup#done'
+
+  resources :sell do
+    resources :purchase, only: [:show] do
+      collection do
+        get 'show'
+        post 'pay'
+        get 'done'
+      end
+    end
+  end
+
+  namespace :api do
+    resources :sell, only: :new, defaults: { format: 'json' }
+    get '/sell/new_delivery', to: 'sell#new_delivery', defaults: { format: 'json' }
+  end
+
   root 'sell#index'
 
   resources :mypage, only: [:index] do
@@ -42,11 +42,9 @@ Rails.application.routes.draw do
       end
     end
   end
-  
+
   get 'mypage/identification'
   get '/mypage', to: 'mypage#index'
-  get 'mypage/card'
-  get '/mypage/card/create', to: 'mypage#card_create'
   get '/mypage/profile', to: 'mypage#profile'
   get '/logout', to: 'mypage#logout'
   get 'mypage/listing', to: 'mypage#product_status'
