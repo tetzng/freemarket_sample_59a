ddc5d7d7a19b27b756f6d4638c75220233f3b0be
yuuspe <pzugn@yahoo.co.jp>
2019-10-10 17:03:32 +0900

    Update delete-product.js

--- app/assets/javascripts/delete-product.js
+++ app/assets/javascripts/delete-product.js
@@ -1,9 +1,11 @@
-$(function() {
-  // ボタンをクリックしたら発動
-  $('#delete-button').on('click', function() {
-    $('#item-edit-content__delete-confirm').addClass('item-edit-content__deleteon');
-  });
-  $('#delete-button-cancel').on('click', function() {
-    $('#item-edit-content__delete-confirm').removeClass('item-edit-content__deleteon');
+$(document).on('turbolinks:load', function(){
+  $(function() {
+    // ボタンをクリックしたら発動
+    $('#delete-button').on('click', function() {
+      $('#item-edit-content__delete-confirm').addClass('item-edit-content__deleteon');
+    });
+    $('#delete-button-cancel').on('click', function() {
+      $('#item-edit-content__delete-confirm').removeClass('item-edit-content__deleteon');
+    });
   });
 });
