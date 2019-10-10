91df696e5fc7749fa096658459f6c4ff6abc3105
seicode <nv7423@gmail.com>
2019-10-10 14:18:07 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_01_001308) do
+ActiveRecord::Schema.define(version: 2019_10_02_082140) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,14 @@ ActiveRecord::Schema.define(version: 2019_10_01_001308) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "customer_id", null: false
+    t.string "card_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -81,7 +89,7 @@ ActiveRecord::Schema.define(version: 2019_10_01_001308) do
     t.string "address1", null: false
     t.string "address2"
     t.string "telephone"
-    t.integer "payment_card_no", null: false
+    t.string "payment_card_no", null: false
     t.integer "paymentmonth_id", null: false
     t.integer "paymentyear_id", null: false
     t.integer "payment_card_security_code", null: false
