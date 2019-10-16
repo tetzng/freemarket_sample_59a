68215defea238e347a7fbc2678f9548d28d85409
enahamada <chamaburiburi11.10@gmail.com>
2019-10-16 13:37:51 +0900

    Update error-message.js

--- app/assets/javascripts/error-message.js
+++ app/assets/javascripts/error-message.js
@@ -4,14 +4,25 @@ $(document).on('turbolinks:load',function(){
     $(".sell-main__form").validate({
         rules: {
             "product[images][]": { required: true },
-            "product[name]": { required: true, max: true },
-            "product[description]": { required: true, text_max: true },
-            "product[category_id]": { required: true, category: true },
+            "product[name]": { required: true, maxlength: 40 },
+            "product[description]": { required: true, maxlength: 1000 },
+            "product[category_id]": { required: true },
             "product[condition_id]": { required: true },
             "product[delivery_charge_id]": { required: true },
             "product[prefecture_id]": { required: true },
             "product[delivery_days_id]": { required: true },
-            "product[price]": { price: true, required: true }
+            "product[price]": { required: true, minlength: 300, maxlength: 9999999 }
+        },
+        messages: {
+            "product[images][]": { required: "画像がありません" },
+            "product[description]": { maxlength: "1000文字以下で入力してください。"　},
+            "product[category_id]":　{ reguired: true, choice: "選択してくだい"},
+            "product[price]": { required: "300からまで9999999の値を入力してください" },
+            "product[category_id]": { required: "選択してください" },
+            "product[condition_id]": { required: "選択してください" },
+            "product[delivery_charge_id]": { required: "選択してください" },
+            "product[prefecture_id]": { required: "選択してください" },
+            "product[delivery_days_id]": { required: "選択してください" }
         }
     });
 });
