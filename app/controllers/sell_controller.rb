ca7467381708c2673fc098bcc26592b0c5012acf
seicode <nv7423@gmail.com>
2019-10-23 23:23:34 +0900

    updated the controller

--- app/controllers/sell_controller.rb
+++ app/controllers/sell_controller.rb
@@ -14,8 +14,10 @@ class SellController < ApplicationController
     @category = Category.all
 
     @main_category = Category.where(sub: '0')
-    @categories = Category.where(sub: params[:id], sub_sub: params[:sub_sub])
+    @categories = Category.where(sub: params[:sub], sub_sub: '0')
     @sub_sub_categories = Category.where(sub: params[:sub_sub], sub_sub: params[:sub] )
+    # @categories = Category.where(sub: params[:id], sub_sub: params[:sub_sub])
+    # @sub_sub_categories = Category.where(sub: params[:sub_sub], sub_sub: params[:sub] )
     respond_to do |format|
       format.html
       format.json
