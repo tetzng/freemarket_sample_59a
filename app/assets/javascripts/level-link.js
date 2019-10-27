bf7b42c0beffe7bdfa8417c59d56921c2e8d0622
seicode <nv7423@gmail.com>
2019-10-27 14:44:49 +0900

    Update level-link.js

--- app/assets/javascripts/level-link.js
+++ app/assets/javascripts/level-link.js
@@ -6,35 +6,52 @@ $(document).on("turbolinks:load", function() {
   //   $(".first-category__wrapper").hide()
   // });
 
-  // 第二階層プルダウン
-  // $(".first-category__wrapper").hover(function () {
-  //   $(".second-category__wrapper").show()
-  //   }, function () {
-  //   $(".second-category__wrapper").hide()
-  // });
-  $("li.first-category").hover(function () {
-    $(this).addClass('active-list')
-    }, function () {
-    $(this).removeClass('active-list')
-    let firstId = $(this).attr('data-category-id')
 
- 
+    // 第二階層カテゴリー表示テンプレ
+    function showSubCategory(data){
+      let showSub = `<li class="second-category" data-category-id="${data.id}" data-category-sub="${data.sub}" data-category-sub-sub="${data.sub_sub}">
+                    <div>
+                    ${data.name}
+                      </div>
+                      </li>`;
+                    $(".second-category__wrapper").append(showSub);
+    }
+
+    // 第三階層カテゴリー表示テンプレ
+    function showSubSubCategory(subData){      
+      let showSubSub = `<li class="third-category" data-category-id="${subData.id}" data-category-sub="${subData.sub}" data-category-sub-sub="${subData.sub_sub}">
+                    <div>
+                    ${subData.name}
+                      </div>
+                      </li>`;
+                    $(".third-category__wrapper").append(showSubSub);
+                    $(".third-category__wrapper[data-category-sub-sub="0"]).remove()
+    }
+      
+
+  // 第二階層プルダウン
+  $("li.first-category").hover(function () {
+    let mainId = $(this).attr('data-category-id');
+    console.log(mainId)
+    // $(this).addClass('active-list', function(){
+    // }, function () {
+    // $(this).removeClass('active-list');
+    // });
+
       $.ajax({
       url: '/sell',
       type: 'GET',
-      data: { sub: firstId },
+      data: { sub: mainId },
       dataType: 'json',
     })
 
-      .done(function(){
-      $(this).addClass('active-list')
-      //   console.log(this)
-      // }, function () {
-      // $(this).removeClass('active-list')
-      // }, function () {
-      // $(".second-category__wrapper").show()
-      // }, function () {
-      // $(".second-category__wrapper").hide()
+      .done(function(sub_categories){
+        $('.second-category').remove();
+        $('.third-category').remove();
+        sub_categories.forEach(function(data){
+        showSubCategory(data)
+        })
+        $(".second-category__wrapper").show();
       })
 
       .fail(function(){
@@ -44,39 +61,52 @@ $(document).on("turbolinks:load", function() {
 
 
 
-  //   $(this).children("li").show();
-  //   let category = $(this).children("li")
-  //   }, function () {
-  //   $(".second-category").hide()
-  // });
-
-
-
   // 第三階層プルダウン
-  // $(".second-category__wrapper").hover(function () {
+  $(document).on({
+    mouseenter: function() {
+      let subId = $(this).attr('data-category-sub');
+      let subSubId = $(this).attr('data-category-id');
+      let subData = {sub: subId,
+                    sub_sub: subSubId};
+      console.log(this);
+      console.log(subSubId);
 
-  //   $(".third-category").show()
-  //   }, function () {
-  //   $(".third-category").hide()
-  // });
+      $.ajax({
+        url: '/sell',
+        type: 'GET',
+        data: subData,
+        dataType: 'json',
+      })
+  
+        .done(function(sub_sub_categories){
+          $('.third-category').remove();
+          $('.third-category__wrapper').show()
+          , function () {
+          $('.third-category__wrapper').hide()
+          };
+          sub_sub_categories.forEach(function(subData){
+          showSubSubCategory(subData)
+          })
+        })
+
+        .fail(function(){
+          alert('カテゴリーがありません');
+        })
+
+    },
+    mouseleave: function() {
+      let subSubId = $(this).attr('data-category-id');
+      console.log(subSubId);
+    }
+
+    }, 'li.second-category')
   
   // 各カテゴリーとクラスの紐付け
   // const firstCategory = 'li.first-category';
   // const secondCategory = 'li.second-category';
   // const thirdCategory = 'li.third-category';
 
-  // 第二階層カテゴリー表示テンプレ
-  // function showSubCategory(data){
-  //   let showSub = `<li class="second-category" data-category-id="" data-category-sub="${data.id}" data-category-sub-sub="${data.sub_sub}">
-  //                 <div>
-  //                 ${data.name}
-  //                  </div>
-  //                  <ul class="third-category__wrapper">
-  //                  <li class="third-category" data-category-id="" data-category-sub="" data-category-sub_sub="">
-  //                  ${data.name}
-  //                  </li>`;
-  //                 $(".second-category__wrapper").append(showSub);
-  // }
+
 
   // 第二階層マウスオーバー表示
   // $("li.first-category").hover(function () {
@@ -91,44 +121,7 @@ $(document).on("turbolinks:load", function() {
 
 
   
-    // let firstId = $(this).attr('data-category-id');
-    // console.log(this);
-    // $(this).addClass('active-list')
-    // , function () {
-    // $(this).removeClass('active-list')
-    // }
 
-  // $("ul.first-category__wrapper").hover(function () {
-
-    // let firstId = $(this).attr('data-category-id');
-    // let secondId = $(this).attr('data-category-sub');
-    // let thirdId = $(this).attr('data-category-sub-sub');
-    // let data = {id: firstId,
-    //             sub: secondId,
-    //             sub_sub: thirdId };
-
-    // $.ajax({
-    //   url: '/sell',
-    //   type: 'GET',
-    //   data: { sub: firstId },
-    //   dataType: 'json',
-    // })
-
-    //   .done(function(categories){
-    //     // $('.li.first-category').removeClass('active-list');
-    //     $(".second-category__wrapper").show()
-    //     , function () {
-    //     $(".second-category__wrapper").hide()
-    //     };
-    //     categories.forEach(function(data){
-    //     showSubCategory(data)
-    //     })
-    //   })
-
-    //   .fail(function(){
-    //     alert('カテゴリーがありません');
-    //   });
-    // });
 
 // ヘッダー「ブランドから探す」プルダウン
   $(".toppage-header-top__footer-search-brand").hover(function () {
