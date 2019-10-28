a551b7eb8f4db6e415eacbd2ccb2aac7243ea7ce
seicode <nv7423@gmail.com>
2019-10-28 19:18:00 +0900

    Update sell_controller.rb

--- app/controllers/sell_controller.rb
+++ app/controllers/sell_controller.rb
@@ -14,14 +14,6 @@ class SellController < ApplicationController
   def index
     @products = Product.limit(10).order('created_at DESC')
     @category = Category.all
-
-    # @main_categories = Category.where(sub: '0')
-    # @sub_categories = Category.where(sub: params[:parent], sub_sub: '0') - Category.where(sub: '0')
-    # @sub_sub_categories = Category.where(sub: params[:child], sub_sub: params[:grandChild]) - Category.where(sub_sub: '0')
-    # respond_to do |format|
-    #   format.html
-    #   format.json
-    # end
   end
 
   def new
@@ -139,5 +131,6 @@ class SellController < ApplicationController
     respond_to do |format|
       format.html
       format.json
+    end
   end
 end
